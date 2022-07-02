84b661e7e7d6948b1ac556ec482cfb88f7a6321a
Noah <80379684+noahdotpy@users.noreply.github.com>
2022-07-02 11:20:59 +1000

    🖱️ Create clicker model and add basic .gitignore

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Clicker
+admin.site.register(Clicker) # Register clicker model onto the admin interface.
--- game/models.py
+++ game/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Clicker(models.Model):
+    clicks = models.IntegerField(default=0)
